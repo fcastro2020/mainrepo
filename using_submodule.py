2f568d31e03b6058116fc0d68359e41ce5249424
fcastro2020 <63263569+fcastro2020@users.noreply.github.com>
2023-08-22 09:52:00 -0300

    Create using_submodule.py
    This is the code that will live in the main module. Meaning that will be using submodules from other repos.

---
+++ using_submodule.py
@@ -0,0 +1,10 @@
+from math-operations import MyMathOperations
+
+# Instantiating the class with a factor of 2.
+myMath = MyMathOperations(2)
+
+r1 = myMath.add_numbers(7, 8)
+r2 = myMath.multiply_numbers(3, 7)
+
+print('The result of my_addition is: ', r1)
+print('Te result of my_multiplication is:', r2)
